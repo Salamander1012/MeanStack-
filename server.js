21f27e1dad59599eb72c1b654896defe50215498
Salman <salmanfakhri2@gmail.com>
2015-08-30 01:47:39 -0400

    added token based auth to access api

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var mongoose = require('mongoose');
 var port = process.env.PORT || 8080;
 var User = require('./app/models/user');
 
+
 //APP CONFIGURATION ----------
 //use body parser to grab info from POST requests
 app.use(bodyParser.urlencoded({extended:true}));
@@ -37,12 +38,143 @@ app.get('/', function(req, res) {
 //get an instance of the express router
 var apiRouter = express.Router();
 
+
+//--------------NODE AUTHENTICATION----------------//
+
+var jwt = require('jsonwebtoken');
+
+var superSecret = 'omgilovechipotlechipotleismylife'
+
+//route for authenticating users
+/*(IDK WHY THIS ISNT WORKING)
+apiRouter.post('/authenticate', function(req, res) {
+	//find user
+	//select name user name and password explicitly
+	User.findOne({
+		username: req.body.username
+	}).select('name username password').exec(function(err, user) {
+		if (err) throw err;
+
+		//no  user with that user name is found
+		if (!user) {
+			res.json({sucess: false, message: 'Authentication failed. User not found'});
+		}
+
+		else if (user) {
+			//check if password matches
+			var validPassword = user.comparePassword(req.body.password);
+			if (!validPassword) {
+				res.json({success: false, message: 'Authentication failed. Wrong password'})
+			}
+		}
+
+		else {
+			//if user is found and password is right
+			//create a token
+			var token = jwt.sign({
+				name: user.name,
+				username: user.username,
+			}, superSecret, {
+				expiresInMinutes: 1440 // expires in 24 hours
+			});
+
+			//return info and json token
+			res.json({success: true, message: 'Enjoy your token!', token: token});
+		}
+
+	})
+})
+
+*/
+//ATTEMPT #2 lol ofcourse this works and the last one doesnt
+apiRouter.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    username: req.body.username
+  }).select('name username password').exec(function(err, user) {
+
+    if (err) throw err;
+
+    // no user with that username was found
+    if (!user) {
+      res.json({ 
+        success: false, 
+        message: 'Authentication failed. User not found.' 
+      });
+    } else if (user) {
+
+      // check if password matches
+      var validPassword = user.comparePassword(req.body.password);
+      if (!validPassword) {
+        res.json({ 
+          success: false, 
+          message: 'Authentication failed. Wrong password.' 
+        });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign({
+        	name: user.name,
+        	username: user.username
+        }, superSecret, {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
+});
+
+//middle ware to verify a token
+
+apiRouter.use(function(req, res, next) {
+	//check header, url params or post params for token
+	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+	//decode token
+	if (token) {
+		//verifies secret and checks exp
+		jwt.verify(token, superSecret, function(err, decoded) {
+			if (err) {
+				return res.status(403).send({success: false, message: 'failed to authenticate token'})
+			}
+
+			else {
+				//if everything is good save to request for use in other routes
+				req.decoded = decoded;
+				next();
+			}
+		})
+	}
+
+	else {
+		//if no token return 403(access forbidden) error message
+		return res.status(403).send({success: false, message: 'No token provided'});
+	}
+})
+
+// api endpoint to get user info
+
+apiRouter.get('/me', function(req, res) {
+	res.send(req.decoded);
+})
+
 //middleware to use for all reqs
 apiRouter.use(function(req, res, next) {
 	//do logging
 	console.log('somebody just came to my app!');
 	next();
 });
+
 
 //test route to make sure everything is working
 apiRouter.get('/', function(req, res) {
@@ -124,6 +256,17 @@ apiRouter.route('/users/:user_id')
 			res.json({message: 'Succesfully deleted!'});
 		});
 	}); 
+
+
+
+
+
+
+
+
+
+
+
 
 
 //REGISTER ROUTES
